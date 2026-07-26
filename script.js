af0ad1e755cf4be3e59e982ba00ba5895fb0d968
rahul khawshi <khawashirahul@gmail.com>
2026-07-27 02:21:21 +0530

    update js file with deployed link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:2200";
+  const API_BASE = "https://persona-mental-health-prediction.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
